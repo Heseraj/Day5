db9360bb2cfda3c63659425590a462ffca6d2f0f
HeSer <B@nd@r1359>
2023-01-07 13:06:14 -0500

    update v3

--- Day5.py
+++ Day5.py
@@ -65,11 +65,101 @@ for height in height_list:
         max_height = max_height
 print(f"The highest student in the group is {max_height} cm tall! ")
 
+#%%  range function
+
+tot_sum = 0
+for number in range(20, 30):
+    tot_sum += number
+print(tot_sum)
+
 #%%
+
+tot_even = 0
+for number in range(1, 101):
+    if number % 2 == 0:
+        tot_even += number
+    else:
+        tot_even = tot_even
+print(tot_even)
+
+#%% Fizz Buzz Interview coding questions
+
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("fizzbuzz")
+    elif number % 3 == 0:
+        print("fizz")
+    elif number % 5 == 0:
+        print("buzz")
+    else:
+        print(number)
+#%% Generating Passwords Will figure this later.
+# import random
+# letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+# numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+# symbols = ["!", "#", "$", "%", "(", ")", "*", "+"]
+#
+# print(f"len of letters is {len(letters)}, numbers is {len(numbers)} and symbols is {len(symbols)}")
+#
+# print("Welcome to your password generateor! ")
+# nr_letters = int(input("how many letters do you like your password to have? \n"))
+# nr_numbers = int(input("how many number do you like your password to have? \n"))
+# nr_symbols = int(input("how many symbols do you like your password to have? \n"))
+#
+# let_list = ""; num_list = ""; sym_list = ""; password_easy = ""
+#
+# for i in range(0, nr_letters):
+#     letter = random.randint(0, len(letters))
+#     # print(letters[letter])
+#     # let_list += letters[letter]
+#     password_easy.append(letter)
+#     print(password_easy)
+#     # print(let_list)
+# for i in range(0, nr_numbers):
+#     number = random.randint(0, len(numbers))
+#     num_list += str(numbers[number])
+# for i in range(0, nr_symbols):
+#     symbol = random.randint(0, len(symbols))
+#     sym_list += str(symbols[symbol])
+# #     print(sym_list)
+# print(let_list)
+# print(num_list)
+# print(sym_list)
+#
+#
+#
+# # for i in range(0, nr_letters):
+# #     random.randint()
+
 #%%
-#%%
-#%%
-#%%
+
+import random
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+symbols = ["!", "#", "$", "%", "(", ")", "*", "+"]
+
+print(f"len of letters is {len(letters)}, numbers is {len(numbers)} and symbols is {len(symbols)}")
+nr_letters = int(input("how many letters do you like your password to have? \n"))
+nr_numbers = int(input("how many number do you like your password to have? \n"))
+nr_symbols = int(input("how many symbols do you like your password to have? \n"))
+
+pass_list = []
+
+for letter in range(1, nr_letters + 1):
+    pass_list.append(random.choice(letters))
+for number in range(1, nr_numbers + 1):
+    pass_list.append(random.choice(numbers))
+for symbol in range(1, nr_symbols):
+    pass_list.append(random.choice(symbols))
+print(pass_list)
+random.shuffle(pass_list)
+print(pass_list)
+
+final_pass = ""
+for char in pass_list:
+    final_pass += char
+print(f"your final strong password is: {final_pass}")
+
 #%%
 #%%
 #%%
